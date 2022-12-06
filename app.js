65a5cca9bff77f576a60a0382a1a02a40cb563dd
RAO YANG <lightrao@hotmail.com>
2022-12-06 13:18:32 +0800

    Adding Express.js

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-// ./app.js
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  console.log("Incoming request...");
-  console.log("Request http method:", req.method);
-  console.log("Request url:", req.url);
+const app = express();
 
-  if (req.method === "POST") {
-    let body = "";
-    req.on("end", () => {
-      console.log("POST request data:", body);
-      const userName = body.split("=")[1];
-      res.end(`<p>Got the POST request!, The username is: ${userName}</p>`);
-    });
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
+// every incoming request or senting back response
+// is funneled through a bunch of middleware functions
+
+// you call next function if you don't want to send the response in
+// this middleware but to forward the request to the next middleware in line.
+
+// If you're not calling next, then any middleware after this middleware, if we had more than one, will not
+// be reached by this request.
+
+app.use((req, res, next) => {
+  let body = "";
+  req.on("end", () => {
+    const userName = body.split("=")[1];
+    if (userName) {
+      req.body = { name: userName };
+    }
+    next();
+  });
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+});
+
+app.use((req, res, next) => {
+  if (req.body) {
+    res.send(`<p>Got the POST request!, 
+    The username is: ${req.body.name}</p>`);
   } else {
-    res.setHeader("Content-Type", "text/html");
-    res.end(`
+    res.send(`
   <form method="POST">
     <input type="text" name="username" />
     <button type="submit">Create User</button>
   </form>
-  `);
+    `);
   }
 });
 
-server.listen(5000);
+app.listen(5000, () => {
+  console.log("Server running at port 5000...");
+});
---
+++ appOnlyNode.js
@@ -0,0 +1,31 @@
+// ./app.js
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("Incoming request...");
+  console.log("Request http method:", req.method);
+  console.log("Request url:", req.url);
+
+  if (req.method === "POST") {
+    let body = "";
+    req.on("end", () => {
+      console.log("POST request data:", body);
+      const userName = body.split("=")[1];
+      res.end(`<p>Got the POST request!, 
+      The username is: ${userName}</p>`);
+    });
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.end(`
+  <form method="POST">
+    <input type="text" name="username" />
+    <button type="submit">Create User</button>
+  </form>
+  `);
+  }
+});
+
+server.listen(5000);
